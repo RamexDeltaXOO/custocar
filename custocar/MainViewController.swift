e240f42d60033cf34f4f2063fc37f069b11ab679
agence-369 <contact@agence369.fr>
2023-01-19 18:25:34 +0100

    Ajoute système pour démarrer avec Xcode 9

---
+++ custocar/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  custocar
+//
+//  Created by AGENCE-369 on 21/12/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("loaded")
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("loaded")
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
